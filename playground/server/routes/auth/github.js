16a9dd1bb59a062c02ca28a3a1681a22e3cfc10e
Ben Chong <bensrww@gmail.com>
2023-03-07 00:03:27 +0800

    add github oauth

--- playground/server/routes/auth/github.js
+++ playground/server/routes/auth/github.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 
+const passport = require('passport');
+
 const router = Router();
 
 module.exports = () => {
@@ -7,13 +9,25 @@ module.exports = () => {
    * GET route to initiate GitHub authentication flow
    * @todo: Implement
    */
-  router.get('/', async (req, res, next) => {
+  router.get('/', passport.authenticate('github'), async (req, res, next) => {
     try {
       return next('Not implemented!');
     } catch (err) {
       return next(err);
     }
   });
+
+  router.get(
+    '/callback',
+    passport.authenticate('github'),
+    async (req, res, next) => {
+      try {
+        return next('Not implemented');
+      } catch (err) {
+        return next();
+      }
+    }
+  );
 
   return router;
 };
